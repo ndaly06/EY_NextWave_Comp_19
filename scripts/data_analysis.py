016be509a693311b64fc697fd39d467f8830651d
Nial Daly <ndaly06@qub.ac.uk>
2019-04-05 22:41:03 +0100

    train data investigated

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,4 +1,10 @@
 # imports the neccessary modules
 import pandas as pd
 
-# 
+# reads & stores the train data in a pandas dataframe
+df = pd.read_csv("/Users/nialdaly/Documents/ey_nextwave/data/data_train.csv", index_col=None)
+
+# prints out the top 5 rows
+print(df.head(5))
+
+print(df.info())
